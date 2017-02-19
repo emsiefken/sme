aecc46daa0d30b7f7227d1e6d40b91eac81544b0
Angela Rubin <aerubin@bh140d-a1.andrew.cmu.edu>
2017-02-19 14:03:04 -0500

    Initial Commit

---
+++ Ella xcode pageTests/Ella_xcode_pageTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ella_xcode_pageTests.swift
+//  Ella xcode pageTests
+//
+//  Created by Angela Rubin on 2/19/17.
+//  Copyright © 2017 Angela Rubin. All rights reserved.
+//
+
+import XCTest
+@testable import Ella_xcode_page
+
+class Ella_xcode_pageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
